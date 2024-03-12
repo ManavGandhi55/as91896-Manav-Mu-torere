554301915aeac5254637cc50520e4e1721c7906f
Manav Gandhi <6793371-ManavGandhi1@users.noreply.replit.com>
2024-03-12 00:45:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Mū tōrere is a Moari board game played on an octagon shaped board, which has 9 spots to place the stones on, one on each corner of the octagon called Kewhai and lines connecting it to the center which is called the Putahi, which is the 9th spot
+print("Welcome to my Mū tōrere game, made my Manav Gandhi")
+
+namelist = []
+player1 = input("Player 1, please enter your name: ")
+player2 = input("Player 2, please enter your name: ")
+namelist.append(player1)
+namelist.append(player2)
+#This is a part of code which asks the players for their playernames, and modifies namelist to include the names of the players.
+print("Welcome", player1, "and", player2, "to the traditional Maori game, Mū tōrere!") 
+#This introduces the game to the players, including their playername variables
+

d20035850b0c161350f0b8e2cd7ff2528ff9f2f8
Manav Gandhi <6793371-ManavGandhi1@users.noreply.replit.com>
2024-03-13 06:01:59 +0000

    2. Added an if else statement which checks the amount player names in namelist then provides an output accordingly

---
+++ dump.py
@@ -0,0 +1,4 @@
+while player1 or player2 not nameunvalid:
+if not isinstance(player1, str) or not isinstance(player2,str): 
+    raise nameunvalid
+    if isinstance(player1, str) and isinstance(player2,str): 
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ player2 = input("Player 2, please enter your name: ")
 namelist.append(player1)
 namelist.append(player2)
 #This is a part of code which asks the players for their playernames, and modifies namelist to include the names of the players.
-print("Welcome", player1, "and", player2, "to the traditional Maori game, Mū tōrere!") 
-#This introduces the game to the players, including their playername variables
-
+if len(namelist) == 2:
+    print("Welcome", namelist[0], "and", namelist[1],
+          "to the traditional Maori game, Mū tōrere!")
+    #This introduces the game to the players, including their playername variables
+else:
+    print("Please enter a valid name for player 1 and 2")
+#this if else statment checks if there are 2 playersnames inputed into the code, and if not, it asks the user to enter a valid name for player 1 and 2.

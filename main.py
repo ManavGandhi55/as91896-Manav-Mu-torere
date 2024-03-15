d12300a5aea4021f9eec76f6c4bac064cf22fece
Manav Gandhi <6793371-ManavGandhi1@users.noreply.replit.com>
2024-03-15 00:35:16 +0000

    3. Board code, spots

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 #Mū tōrere is a Moari board game played on an octagon shaped board, which has 9 spots to place the stones on, one on each corner of the octagon called Kewhai and lines connecting it to the center which is called the Putahi, which is the 9th spot
+
+
+
+spots = {
+    1: "A",
+    2: "A",
+    3: "A",
+    4: "A",
+    5: "B",
+    6: "B",
+    7: "B",
+    8: "B",
+    9: "0"
+}
+
+def board():
+    print('\n    ' + spots[8] + '8  |  ' + spots[1] + '1   ')
+    print('   \\         /')
+    print('  ' + spots[7] + '7         ' + spots[2] + '2')
+    print('  --    ' + spots[9] + '9   --')
+    print('  ' + spots[6] + '6         ' + spots[3] + '3   ')
+    print('   /         \\')
+    print('    ' + spots[5] + '5  |  ' + spots[4] + '4   ')
+        
+
+board()
+
 print("Welcome to my Mū tōrere game, made my Manav Gandhi")
 
 namelist = []
@@ -14,3 +41,4 @@ if len(namelist) == 2:
 else:
     print("Please enter a valid name for player 1 and 2")
 #this if else statment checks if there are 2 playersnames inputed into the code, and if not, it asks the user to enter a valid name for player 1 and 2.
+

69f29a8107ddd44d02c28c8b0d7e3808d7d2a8ce
Manav Gandhi <6793371-ManavGandhi1@users.noreply.replit.com>
2024-03-22 00:36:09 +0000

    11. cleaned up hueristics, pivoted player move stuff

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ spots = {
 }
 # The dictionary above stores the starting positions of the Perpere on the board.
 
-secspots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+sec_spots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 #This list stores the spots that are available to be played on.
 
 
-def mainboard():
+def main_board():
     print('\n    ' + spots[8] + '8  --  ' + spots[1] + '1   ')
     print('   \\          /')
     print('  ' + spots[7] + '7          ' + spots[2] + '2')
@@ -38,21 +38,20 @@ def mainboard():
 #This function prints the main board.
 
 
-def secondboard():
-    print('\n     ' + secspots[7] + '  -  ' + secspots[0] + '   ')
+def second_board():
+    print('\n     ' + sec_spots[7] + '  -  ' + sec_spots[0] + '   ')
     print('    \\       /')
-    print('   ' + secspots[6] + '         ' + secspots[1] + '')
-    print('   -    ' + secspots[8] + '    -')
-    print('   ' + secspots[5] + '         ' + secspots[2] + '   ')
+    print('   ' + sec_spots[6] + '         ' + sec_spots[1] + '')
+    print('   -    ' + sec_spots[8] + '    -')
+    print('   ' + sec_spots[5] + '         ' + sec_spots[2] + '   ')
     print('    /       \\')
-    print('     ' + secspots[4] + '  -  ' + secspots[3] + '   ')
+    print('     ' + sec_spots[4] + '  -  ' + sec_spots[3] + '   ')
 
 
 #This function prints the second board which holds the spots that are available.
 
-player1 = 1
-player2 = 2
-#sets who is player 1 and player 2.
+current_player = 0
+#This variable stores the current player.
 
 players = {1: "B", 2: "R"}
 #This dictionary stores the players and to there perepere colour.
@@ -61,35 +60,34 @@ quit = False
 playing = True
 #boolean variables for the while loop.
 
-player1moves = []
-player2moves = []
-namelist = []
+name_list = []
 #empty lists for playermoves and namelist before the while loop starts
 
-can_movep1 = 0
-can_movep2 = 0
+can_move = 0
 
+def moved_piece():
+    
 
 def introduction():
     print("Welcome to my Mū tōrere game, made my Manav Gandhi")
 
-    namelist = []
+    name_list = []
     player1 = input("Player 1, please enter your name: ")
     player2 = input("Player 2, please enter your name: ")
-    namelist.append(player1)
-    namelist.append(player2)
+    name_list.append(player1)
+    name_list.append(player2)
     #This is a part of code which asks the players for their playernames
     #modifies namelist to include the names of the players.
-    if len(namelist) == 2:
-        print("Welcome", namelist[0], "and", namelist[1],
+    if len(name_list) == 2:
+        print("Welcome", name_list[0], "and", name_list[1],
               "to the traditional Maori game, Mū tōrere!")
         #This introduces the game to the players, including their playername variables
     else:
         print("Please enter a valid name for player 1 and 2")
         #This if else statement checks if there are 2 player names inputted into the code.
         #and if not, it asks the user to enter a valid name for player 1 and 2.
-    rulescheck = input("Do you know the rules of Mu torere: ").lower()
-    if rulescheck == "yes":
+    rules_check = input("Do you know the rules of Mu torere: ").lower()
+    if rules_check == "yes":
         print("Okay lets get started then...")
     else:
         print("The rules to Mu torere are very simple...")
@@ -97,8 +95,8 @@ def introduction():
             "The game is played on an 8-sided board, with 9 spots to place the Perepers's on as the spot in the middle (the putahi) is the 9th spot. Each player takes turns moving to an unoccupied spot on the board. Two players cannot occupy the same spot at the same time. In order to win, you need to block the opposing player from being able to move."
         )
         #This is the rules of the game, which it asks the player if they already know them or not.
-        rulesfinish = input("Are you finished reading the rules?: ").lower()
-        if rulesfinish == "yes":
+        rules_finish = input("Are you finished reading the rules?: ").lower()
+        if rules_finish == "yes":
             print("Okay lets get started then...")
         else:
             print("Well you'll figure it out along the way...")
@@ -113,29 +111,32 @@ while playing is True:
 
     introduction()
 
-    mainboard()
+    main_board()
     #prints main board
 
-    secondboard()
+    second_board()
     #prints second board
-    player1move = input(namelist[0] + ", please enter your move: ").lower()
-    if player1move == "quit":
-        print(
-            "Oh, I see how it is, you're quitting... You know quitters are the weakest form of homosapiens."
-        )
-        time.sleep(2)
-        os.system('cls' if os.name == 'nt' else 'clear')
-    else:
-        player1move = int(player1move)
-        #asks player 1 to enter their move.
-        if spots[player1move] == players[1] or spots[player1move] == players[2]:
-            can_movep1 = False 
+    if current_player == 0:
+        move = input("Player 1, please enter the number of the spot you would like to move to: ")
+        if move == "quit":
+            print("Oh, I see how it is, you're quitting... You know quitters are the weakest form of homosapiens.")
+            
+            time.sleep(2)
+            os.system('cls' if os.name == 'nt' else 'clear')
         else:
-            can_movep1 = True
-        if can_movep1 is True:
-            player1moves.append(player1move)
-            spots[player1move] = players[1]
-        else:
-            print("You cannot move there, please try again.")
-            player1move = int(input(namelist[0] +
-                                    ", please enter your move: "))
+            move = int(move)
+            #asks player 1 to enter their move.
+            if spots[move] == players[1] or spots[move] == players[2]:
+                can_move = False 
+            else:
+                can_move = True
+                
+            if can_move is True:
+                
+                spots[move] = players[1]
+                main_board()
+            else:
+                print("You cannot move there, please try again.")
+                move = input("Player 1, please enter the number of the spot you would like to move to: ")
+    elif current_player == 1:
+        

b280f57a011ce06e70c38ebd97a78405d7497085
ManavGandhi55 <164471254+ManavGandhi55@users.noreply.github.com>
2024-03-24 02:37:33 +0000

    14.  Player move function works functionaly.

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def second_board():
 
 #This function prints the second board which holds the spots that are available.
 
-
 players = {1: "B", 2: "R"}
 #This dictionary stores the players and to there perepere colour.
 
@@ -61,58 +60,95 @@ win = False
 
 name_list = []
 player_moves = []
+
 #empty lists for playermoves and namelist before the while loop starts
 
+player_move = 0
 can_move = 0
 
 
 def introduction():
-    print("Welcome to my Mū tōrere game, made my Manav Gandhi")
-
-    name_list = []
+    print("Welcome to my Mū tōrere game, made by Manav Gandhi")
     player1 = input("Player 1, please enter your name: ")
     player2 = input("Player 2, please enter your name: ")
     name_list.append(player1)
     name_list.append(player2)
-    #This is a part of code which asks the players for their playernames
-    #modifies namelist to include the names of the players.
-    print("Welcome", name_list[0], "and", name_list[1], "to the traditional Maori game, Mū tōrere!")        
-    #This introduces the game to the players, including their playername variables
+    # This is a part of code which asks the players for their playernames
+    # Modifies name_list to include the names of the players.
+    print("Welcome", name_list[0], "and", name_list[1],
+          "to the traditional Maori game, Mū tōrere!")
+    # This introduces the game to the players, including their playername variables
     rules_check = input("Do you know the rules of Mu torere: ").lower()
     if rules_check == "yes":
-        print("Okay lets get started then...")
+        print("Okay let's get started then...")
     else:
         print("The rules to Mu torere are very simple...")
         print(
             "The game is played on an 8-sided board, with 9 spots to place the Perepers's on as the spot in the middle (the putahi) is the 9th spot. Each player takes turns moving to an unoccupied spot on the board. Two players cannot occupy the same spot at the same time. In order to win, you need to block the opposing player from being able to move."
         )
-        #This is the rules of the game, which it asks the player if they already know them or not.
+        # This is the rules of the game, which it asks the player if they already know them or not.
         rules_finish = input("Are you finished reading the rules?: ").lower()
         if rules_finish == "yes":
-            print("Okay lets get started then...")
+            print("Okay let's get started then...")
         else:
             print("Well you'll figure it out along the way...")
-        #Asks if they're finished reading the rules to Mu torere.
+        # Asks if they're finished reading the rules to Mu torere.
     print("The game will now begin...")
-
-#Start of game
-
-introduction()
+    return name_list
 
 
-while win is False:
-	os.system('cls' if os.name == 'nt' else 'clear')
-	#clears screen to remove excessive clutter
-	main_board()
-	#prints main board		
-	second_board()
-	#prints second board
-	turn += 1
-	if turn > 2:
-		turn = 1		
-	
-	os.system('cls' if os.name == 'nt' else 'clear')
-	
-	
-		
-		
+def move_logic():
+    global player_moves
+    player_move = int(
+        input((name_list[turn - 1], "(", players[turn],
+               ") What piece would you like to move?")))
+    player_moves.append(player_move)
+    new_position = moved_piece(player_move, spots)
+    if new_position is not None:
+        spots[new_position] = players[turn]
+        spots[player_move] = "0"
+    else:
+        print("Invalid move. Please try again.")
+
+
+def moved_piece(player_move, spots):
+    connecting_spots = {
+        1: [8, 2, 9],
+        8: [7, 1, 9],
+    }
+
+    if player_move in connecting_spots:
+        available_spots = connecting_spots[player_move]
+        for spot in available_spots:
+            if spots[spot] == '0':
+                return spot
+    elif player_move == 9:
+        for zero in spots:
+            if spots[zero] == '0':
+                return zero
+    else:
+        if player_move - 1 in spots and spots[player_move - 1] == '0':
+            return player_move - 1
+        elif player_move + 1 in spots and spots[player_move + 1] == '0':
+            return player_move + 1
+        elif spots[9] == '0':
+            return 9
+        else:
+            return None
+
+
+# Start of game
+name_list = introduction()
+
+while not win:    
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # clears screen to remove excessive clutter
+    main_board()
+    # prints main board
+    second_board()
+    # prints second board
+    move_logic()  
+    # Handle player move
+    
+    turn = 2 if turn == 1 else 1  
+    # Switch player turn
